9f7c324a2673191ef9cfa9bc3002d8a0b52616d2
ivailo-chromeye <ivailo@chromeye.com>
2020-04-30 13:32:50 +0300

    home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { useStaticQuery, graphql, Link } from "gatsby"
-
+import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-import PageHeadlineComponent from "../components/pageHeadline"
 import FeedRaces from "../components/feedRaces"
 import NewsSliderComponent from "../components/NewsSlider"
 import FreeBetsShortform from "../components/FreeBetsShortform"
 import GridLayout from "../components/GridLayout"
 import BettingGuideHomepage from "../components/BettingGuideHomepage"
 
-
 const Homepage = () => {
-    const pageData = useStaticQuery(graphql`
-        query MyQuery {
-            wordpressPage(wordpress_id: {eq: 290}) {
-                id
-                slug
-                title
-                wordpress_id
-                acf {
-                    title
-                    subtitle
-                    betting_text
-                    betting_title
-                    betting_image {
-                        source_url
-                    }
-                }
-            }
-        }   
-    `)
-
-    const pageTitle = pageData.wordpressPage.acf.title
-    const pageSubtitle = pageData.wordpressPage.acf.subtitle
-    
-    const bettinArticle = {
-        'image': pageData.wordpressPage.acf.betting_image,
-        'title': pageData.wordpressPage.acf.betting_title,
-        'text': pageData.wordpressPage.acf.betting_text
-    }
-
     return (
         <>
             <Layout>
                 <SEO title="Home" />
-                {/* <PageHeadlineComponent title={pageTitle} subtitle={pageSubtitle}/> */}
                 <FeedRaces/>
                 <FreeBetsShortform filter="homepage"/>
                 <GridLayout responsiveFull={1024} sideGap={12} equalHeight={true}>
