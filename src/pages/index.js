29bb048462ede4cc65d5548295ade60add601817
ivailo-chromeye <ivailo@chromeye.com>
2020-04-30 16:35:54 +0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FeedRaces from "../components/feedRaces"
-// import NewsSliderComponent from "../components/NewsSlider"
-// import FreeBetsShortform from "../components/FreeBetsShortform"
+import NewsSliderComponent from "../components/newsSlider"
+import FreeBetsShortform from "../components/freeBetsShortform"
 import GridLayout from "../components/GridLayout"
 import BettingGuideHomepage from "../components/BettingGuideHomepage"
 
@@ -15,9 +15,9 @@ const Homepage = () => {
             <Layout>
                 <SEO title="Home" />
                 <FeedRaces/>
-                {/* <FreeBetsShortform filter="homepage"/> */}
+                <FreeBetsShortform filter="homepage"/>
                 <GridLayout responsiveFull={1024} sideGap={12} equalHeight={true}>
-                    {/* <NewsSliderComponent width="60%"/> */}
+                    <NewsSliderComponent width="60%"/>
                     <BettingGuideHomepage width="40%"/>
                 </GridLayout>
             </Layout>
